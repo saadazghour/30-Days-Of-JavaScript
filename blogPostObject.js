129483d7b7aac2fdcd118e5d6b5d65c01a5abec3
Azghour-Saad <saad.azghour@gmail.com>
2022-05-03 19:48:57 +0000

    Add, Post Obj

---
+++ blogPostObject.js
@@ -0,0 +1,11 @@
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
+
+const post = new Post("Title", "Body", "Author");
+console.log(post);

07a6bd50894e38709dc709b683bf1ea5d312995f
Azghour-Saad <saad.azghour@gmail.com>
2022-05-03 20:30:45 +0000

    Price Range Objects

--- blogPostObject.js
+++ blogPostObject.js
@@ -9,3 +9,13 @@ function Post(title, body, author) {
 
 const post = new Post("Title", "Body", "Author");
 console.log(post);
+
+//  Price Range Objects
+const priceRanges = [
+  { label: "$", toolTip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
+  { label: "$$", toolTip: "Moderate", minPerPerson: 11, maxPerPerson: 20 },
+  { label: "$$$", toolTip: "Expensive", minPerPerson: 22, maxPerPerson: 60 },
+];
+
+// Filter features!
+let restaurants = [{ averagePerPerson: 6 }];

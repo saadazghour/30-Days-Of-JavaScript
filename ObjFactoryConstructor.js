9d74c86c5894d23d430b7216721e49b4e1139021
Azghour-Saad <saad.azghour@gmail.com>
2022-05-03 18:52:32 +0000

    Object Equality

--- ObjFactoryConstructor.js
+++ ObjFactoryConstructor.js
@@ -29,5 +29,27 @@ function Constructor(street, city, zipCode) {
 const newConstructor = new Constructor("Morocco", "Marrakesh", "10000");
 console.log(newConstructor);
 
-const secondConstructor = new Constructor();
-console.log(newConstructor.draw());
+const secondConstructor = new Constructor("Morocco", "Marrakesh", "10000");
+console.log(newConstructor);
+
+const thirdConstructor = newConstructor;
+
+// Object Equality
+const areEqual = (newConstructor, secondConstructor) => {
+  return (
+    newConstructor.street === secondConstructor.street &&
+    newConstructor.city === secondConstructor.city &&
+    newConstructor.zipCode === secondConstructor.zipCode
+  );
+};
+
+// Referencing same Object!
+const areSame = (newConstructor, secondConstructor) => {
+  return newConstructor === secondConstructor;
+};
+
+console.log(areEqual(newConstructor, secondConstructor));
+console.log(areSame(newConstructor, secondConstructor));
+
+console.log(areEqual(newConstructor, thirdConstructor));
+console.log(areSame(newConstructor, thirdConstructor));

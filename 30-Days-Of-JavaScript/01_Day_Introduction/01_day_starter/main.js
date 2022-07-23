2123254e91d27b69ec43fd0b695834682fcf0933
Azghour-Saad <saad.azghour@gmail.com>
2022-07-23 17:41:53 +0100

    init

--- 30-Days-Of-JavaScript/01_Day_Introduction/01_day_starter/main.js
+++ 30-Days-Of-JavaScript/01_Day_Introduction/01_day_starter/main.js
@@ -1,1 +1,8 @@
+// @ts-nocheck
 // the variable values can be accessed from other variable.js file
+
+console.log(firstName, lastName, country, age, isMarried); // SAAD AZGHOUR Anywhere 30 false
+
+console.log(gravity, boilingPoint, PI); // 9.8 100 3.14
+
+console.log(name, job, live); // SAAD Software Engineer Everywhere
--- 30-Days-Of-JavaScript/01_Day_Introduction/01_day_starter/variable.js
+++ 30-Days-Of-JavaScript/01_Day_Introduction/01_day_starter/variable.js
@@ -15,7 +15,7 @@ const gravity = 9.8; // earth gravity  in m/s2
 const boilingPoint = 100; // water boiling point, temperature in oC
 const PI = 3.14; // geometrical constant
 
-// Variables can also be declaring in one line separated by comma
+// Variables can also be declaring in one line separated by comma, but it's not recommended!
 
 let name = "SAAD",
   job = "Software Engineer",

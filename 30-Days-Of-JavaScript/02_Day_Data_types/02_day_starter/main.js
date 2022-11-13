f7bd364ad619b37ff86078f9de6d34908d5e6200
Azghour-Saad <saad.azghour@gmail.com>
2022-11-13 22:27:27 +0100

    Add, the first exercise!

--- 30-Days-Of-JavaScript/02_Day_Data_types/02_day_starter/main.js
+++ 30-Days-Of-JavaScript/02_Day_Data_types/02_day_starter/main.js
@@ -393,3 +393,75 @@ console.log(typeof +"10"); // number
 console.log(parseInt("9.81")); // 9
 
 // 1.3 ----------------------- Exercises !!
+// Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+const challenge = "30 Days of Javascript";
+// Print the string on the browser console using console.log()
+console.log(`I will print my Full Name : Saad Azghour`);
+// Print the length of the string on the browser console using console.log()
+console.log(`I will print my Full Name : Saad Azghour`.length);
+// Change all the string characters to capital letters using toUpperCase() method.
+console.log("i will print my full name : saad azghour".toUpperCase());
+// Change all the string characters to lowercase letters using toLowerCase() method
+console.log("I WILL PRINT MY FULL NAME : SAAD AZGHOUR".toLowerCase());
+// Cut (slice) out the first word of the string using substr() or substring() method
+console.log("Saad Azghour".substr(0, 1));
+console.log("Saad Azghour".substring(0, 1));
+// Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log("30 Days Of JavaScript".slice(2));
+// Check if the string contains a word Script using includes() method
+console.log("30 Days Of JavaScript".includes("Script")); // true
+// Split the string into an array using split() method
+console.log("30 Days Of JavaScript".split(""));
+// Split the string 30 Days Of JavaScript at the space using split() method
+console.log("30 Days Of JavaScript".split(" "));
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+console.log(
+  "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(",")
+);
+// Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+console.log("30 Days Of JavaScript".replace("JavaScript", "Python"));
+// What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+console.log("30 Days Of JavaScript".charAt(15));
+// What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+console.log("30 Days Of JavaScript".charCodeAt(15));
+
+// Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+console.log("30 Days Of JavaScript".indexOf("30")); // 0
+// Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+console.log("30 Days Of JavaScript".lastIndexOf("JavaScript")); // 11
+
+// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".indexOf(
+    "because"
+  )
+); // 31
+
+// Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".lastIndexOf(
+    "because"
+  )
+); // 47
+
+// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+console.log(
+  "You cannot end a sentence with because because because is a conjunction".search(
+    "because"
+  )
+); // 31
+
+// Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+console.log(" 30 Days Of JavaScript ".trim());
+// Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log("30 Days Of JavaScript".startsWith("3")); // true
+// Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log("30 Days Of JavaScript".endsWith("t")); // true
+// Use match() method to find all the a in 30 Days Of JavaScript
+console.log("30 Days Of JavaScript".match("a"));
+// Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+console.log("30 Days Of".concat("JavaScript"));
+// Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log("30 Days Of JavaScript".repeat(2));

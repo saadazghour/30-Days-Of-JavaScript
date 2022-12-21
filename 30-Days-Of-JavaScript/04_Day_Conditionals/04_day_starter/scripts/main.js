d476df5861bfbf8061d93bab8cfbb1ae7e02c947
Azghour-Saad <saad.azghour@gmail.com>
2022-12-21 11:19:54 +0100

    Add, Exercises
    Level 1

--- 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -200,3 +200,51 @@ raining
   : console.log("No need for a rain coat.");
 
 // You are extraordinary and you have a remarkable potential. You have just completed day 4 challenges and you are four steps ahead to your way to greatness. Now do some exercises for your brain and muscle!!
+
+// Exercises:
+// Exercises: Level 1!
+
+// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+let userAge = Number(prompt("Enter your age :"));
+// userAge = 18;
+
+if (userAge >= 18) {
+  console.log("You are old enough to drive!"); //
+} else {
+  console.log("You need to wait some years to turn 18.");
+}
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+let yourAge = Number(prompt("Enter your age :"));
+
+yourAge = 30;
+let myAge = 28;
+
+if (yourAge > myAge) {
+  console.log(`You are ${yourAge - myAge} years older than me.
+ `); // You are 2 years older than me.
+} else if (yourAge === myAge) {
+  console.log(`We are Equal!`);
+} else if (yourAge < myAge) {
+  console.log(`You are ${myAge - yourAge} years younger than me.
+ `); // You are 2 years older than me.
+}
+
+// If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+const aNum = 6;
+const bNum = 2;
+
+aNum > bNum
+  ? console.log("a is greater than b") //
+  : console.log("a is less than b");
+
+// Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+
+const evenNum = 6;
+
+evenNum % 2 === 0
+  ? console.log(`${evenNum} is an even number`) // 6 is an even number
+  : console.log(`${evenNum} is is an odd number.`);

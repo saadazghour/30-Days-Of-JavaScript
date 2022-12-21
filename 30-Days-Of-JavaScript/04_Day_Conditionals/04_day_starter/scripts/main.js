f0fbfaaa038f34d2fb558d73132e8507e4817b99
Azghour-Saad <saad.azghour@gmail.com>
2022-12-21 12:41:09 +0100

    Add, Exercises: Level 2!

--- 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 30-Days-Of-JavaScript/04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -248,3 +248,63 @@ const evenNum = 6;
 evenNum % 2 === 0
   ? console.log(`${evenNum} is an even number`) // 6 is an even number
   : console.log(`${evenNum} is is an odd number.`);
+
+// Exercises: Level 2
+// Write a code which can give grades to students according to theirs scores:
+
+// 80 - 100, A;
+// 70 - 89, B;
+// 60 - 69, C;
+// 50 - 59, D;
+// 0 - 49, F;
+
+let score = 100;
+
+if (score >= 80 && score <= 100) {
+  console.log("A"); //
+} else if (score >= 70 && score <= 89) {
+  console.log("B");
+} else if (score >= 60 && score <= 69) {
+  console.log("C");
+} else if (score >= 50 && score <= 59) {
+  console.log("D");
+} else {
+  console.log("F");
+}
+
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+let month = "September";
+
+if (month === "September" || month === "October" || month === "November") {
+  console.log("The season is Autumn"); //
+} else if (
+  month === "December" ||
+  month === "January" ||
+  month === "February"
+) {
+  console.log("The season is Winter");
+} else if (month === "March" || month === "April" || month === "May") {
+  console.log("The season is Spring");
+} else if (month === "June" || month === "July" || month === "August") {
+  console.log("The season is Summer");
+}
+
+// Check if a day is weekend day or a working day. Your script will take day as an input.
+
+let dayUser = String(prompt("What day is today ?", "sunday"));
+// dayUser = "sunday";
+let isDay = dayUser.toLowerCase();
+
+isDay === "saturday" || isDay === "sunday"
+  ? console.log("This is Weekend day!")
+  : console.log("This is working day!");
+
+// Exercises:
+// Exercises: Level 3
+// Write a program which tells the number of days in a month.

892db99ab6aab9abe9d534418e8342b5c8566122
Azghour-Saad <saad.azghour@gmail.com>
2023-04-11 03:58:42 +0000

    Add Exercise: Level 1

--- 30-Days-Of-JavaScript/05_Day_Arrays/05_day_starter/scripts/main.js
+++ 30-Days-Of-JavaScript/05_Day_Arrays/05_day_starter/scripts/main.js
@@ -471,3 +471,92 @@ console.log(fullStack[0]); // [ 'HTML', 'Css', 'JS', 'React', 'Redux' ]
 console.log(fullStack[1]); // [ 'Node', 'Express', 'MongoDB' ]
 
 // --------------  Exercise ------------------
+
+// Exercise: Level 1 :
+
+const countryExercice = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechsExercice = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+// Declare an empty array :
+const emptyArray = [];
+// Declare an array with more than 5 number of elements
+const arrayElements = ["Saad", "Azghour", 5, 10, true, false];
+// Find the length of your array
+console.log(arrayElements.length); // 6
+// Get the first item, the middle item and the last item of the array
+console.log(arrayElements[0]);
+console.log(arrayElements[2]);
+console.log(arrayElements[arrayElements.length - 1]); // false
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = ["Saad", "Azghour", 5, 10, true, false];
+console.log(mixedDataTypes.length); // 6
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+
+// Print the array using console.log()
+console.log(itCompanies);
+// Print the number of companies in the array
+console.log(itCompanies.length);
+// Print the first company, middle and last company
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[itCompanies.length - 1]);
+// Print out each company
+console.log(itCompanies); // TO DO
+// Change each company name to uppercase one by one and print them out
+console.log(itCompanies[0].toUpperCase());
+console.log(itCompanies[1].toUpperCase());
+console.log(itCompanies[2].toUpperCase());
+console.log(itCompanies[3].toUpperCase());
+console.log(itCompanies[4].toUpperCase());
+console.log(itCompanies[5].toUpperCase());
+console.log(itCompanies[6].toUpperCase());
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+console.log(itCompanies.join(", ").concat(" are big IT companies."));
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+itCompanies.includes("Facebook")
+  ? console.log("Facebook")
+  : console.log("Company is not found!");
+// Filter out companies which have more than one 'o' without the filter method
+// TO DO
+// Sort the array using sort() method
+console.log(itCompanies.sort());
+// Reverse the array using reverse() method
+console.log(itCompanies.reverse());
+// Slice out the first 3 companies from the array
+console.log(itCompanies.slice(0, 3));
+// Slice out the last 3 companies from the array
+console.log(itCompanies.slice(-3));
+// Slice out the middle IT company or companies from the array
+
+// Exercise: Level 2 :
+// Exercise: Level 3 :

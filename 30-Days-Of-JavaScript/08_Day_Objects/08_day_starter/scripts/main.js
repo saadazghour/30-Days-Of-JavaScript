c837c1279cfa5aff7e5f1e00c3a40ed36d9b5b5c
Azghour-Saad <saad.azghour@gmail.com>
2023-05-20 19:45:14 +0100

    Exercises: Level 3

--- 30-Days-Of-JavaScript/08_Day_Objects/08_day_starter/scripts/main.js
+++ 30-Days-Of-JavaScript/08_Day_Objects/08_day_starter/scripts/main.js
@@ -777,3 +777,107 @@ for (const country in countries) {
 console.log(countryArray);
 
 // Exercises: Level 3
+
+// Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+
+const personAccount = {
+  firstName: "saad",
+  lastName: "azghour",
+  incomes: {
+    description: "my income",
+    amount: 20,
+  },
+  expenses: {
+    amount: 10,
+    totalIncome: "my total income",
+    totalExpense: "my total expense",
+    accountInfo: "my account info",
+    addIncome: "my add income",
+    addExpense: "my add expense",
+    accountBalance: function name(params) {
+      return personAccount.incomes.amount - this.amount;
+    },
+  },
+};
+
+console.log(personAccount.expenses.accountBalance()); // 10
+
+// **** Questions:2, 3 and 4 are based on the following two arrays: users and products ()
+
+const users = [
+  {
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
+
+const products = [
+  {
+    _id: "eedfcf",
+    name: "mobile phone",
+    description: "Huawei Honor",
+    price: 200,
+    ratings: [
+      { userId: "fg12cy", rate: 5 },
+      { userId: "zwf8md", rate: 4.5 },
+    ],
+    likes: [],
+  },
+  {
+    _id: "aegfal",
+    name: "Laptop",
+    description: "MacPro: System Darwin",
+    price: 2500,
+    ratings: [],
+    likes: ["fg12cy"],
+  },
+  {
+    _id: "hedfcg",
+    name: "TV",
+    description: "Smart TV:Procaster",
+    price: 400,
+    ratings: [{ userId: "fg12cy", rate: 5 }],
+    likes: ["fg12cy"],
+  },
+];
+
+// Imagine you are getting the above users collection from a MongoDB database.
+
+// *** a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+// **** b. Create a function called signIn which allows user to sign in to the application
